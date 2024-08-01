c6a2920c3e3c3900d30cbc7765a9f83d0fea37ab
lomobit <lomobit@gmail.com>
2024-08-01 09:47:52 +0300

    Бенчмарк на проверку пустых строк

---
+++ ForCSharpTesting/Benchmarks/CheckEmptyString.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ForCSharpTesting.Benchmarks;
+
+public class CheckEmptyString
+{
+    [Params(null, "", " ", "Turbocharged: Writing High-Performance C# and .NET Code - Steve Gordon - NDC Oslo 2024")]
+    public string? StringForCheck;
+
+    [Benchmark]
+    public void StringIsNullOrWhiteSpace()
+    {
+        var result = string.IsNullOrWhiteSpace(StringForCheck);
+    }
+
+
+    [Benchmark]
+    public void MemoryExtensionsIsWhiteSpace()
+    {
+        var length = StringForCheck?.Length ?? 0;
+        var result = length == 0 || MemoryExtensions.IsWhiteSpace(StringForCheck);
+    }
+}
--- ForCSharpTesting/Program.cs
+++ ForCSharpTesting/Program.cs
@@ -6,6 +6,6 @@ public class Program
 {
     static void Main(string[] args)
     {
-        
+        BenchmarkRunner.Run(typeof(Program).Assembly);
     }
 }

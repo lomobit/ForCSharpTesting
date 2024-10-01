cf2d533458c9694218885551ec7725c5dc58dfcb
vghslnk <lomobit@gmail.com>
2024-10-01 16:02:57 +0300

    add my deque

---
+++ ForCSharpTesting/LeetCodeSolutions/Common/MyCircularDeque.cs
@@ -0,0 +1,136 @@
+﻿namespace ForCSharpTesting.LeetCodeSolutions.Common;
+
+/// <summary>
+/// https://leetcode.com/problems/design-circular-deque/
+/// </summary>
+public class MyCircularDeque
+{
+    private readonly int[] _internalArray;
+    private int _startIndex = 0;
+    private int _endIndex = 0;
+
+    public int Length { get; set; }
+    public int Capacity { get; }
+
+
+    public MyCircularDeque(int capacity)
+    {
+        Length = 0;
+        Capacity = capacity;
+        _internalArray = new int[capacity];
+    }
+
+    public bool InsertFront(int value)
+    {
+        if (Length == Capacity)
+        {
+            return false;
+        }
+
+        var currentIndex = _startIndex - 1;
+        if (currentIndex < 0)
+        {
+            currentIndex = Capacity - 1;
+        }
+
+        _internalArray[currentIndex] = value;
+        _startIndex = currentIndex;
+        Length++;
+
+        return true;
+    }
+
+    public bool InsertLast(int value)
+    {
+        if (Length == Capacity)
+        {
+            return false;
+        }
+
+        var currentIndex = _endIndex + 1;
+        if (currentIndex >= Capacity)
+        {
+            currentIndex = 0;
+        }
+
+        _internalArray[_endIndex] = value;
+        _endIndex = currentIndex;
+        Length++;
+
+        return true;
+    }
+
+    public bool DeleteFront()
+    {
+        if (Length == 0)
+        {
+            return false;
+        }
+
+        var currentIndex = _startIndex + 1;
+        if (currentIndex >= Capacity)
+        {
+            currentIndex = 0;
+        }
+
+        _startIndex = currentIndex;
+        Length--;
+
+        return true;
+    }
+
+    public bool DeleteLast()
+    {
+        if (Length == 0)
+        {
+            return false;
+        }
+
+        var currentIndex = _endIndex - 1;
+        if (currentIndex < 0)
+        {
+            currentIndex = Capacity - 1;
+        }
+
+        _endIndex = currentIndex;
+        Length--;
+
+        return true;
+    }
+
+    public int GetFront()
+    {
+        if (Length == 0)
+        {
+            return -1;
+        }
+
+        return _internalArray[_startIndex];
+    }
+
+    public int GetRear()
+    {
+        if (Length == 0)
+        {
+            return -1;
+        }
+
+        var currentIndex = _endIndex - 1;
+        if (currentIndex < 0)
+        {
+            currentIndex = Capacity - 1;
+        }
+
+        return _internalArray[currentIndex];
+    }
+
+    public bool IsEmpty()
+    {
+        return Length == 0;
+    }
+
+    public bool IsFull()
+    {
+        return Length == Capacity;
+    }
+}
--- ForCSharpTesting/Program.cs
+++ ForCSharpTesting/Program.cs
@@ -1,6 +1,4 @@
-﻿using BenchmarkDotNet.Running;
-using ForCSharpTesting.Benchmarks;
-using Serilog;
+﻿using ForCSharpTesting.LeetCodeSolutions.Common;
 
 namespace ForCSharpTesting;
 
@@ -8,6 +6,21 @@ public class Program
 {
     static void Main(string[] args)
     {
-        CheckReusableArrays.StartBenchmark();
+        var k = 2;
+
+        MyCircularDeque obj = new MyCircularDeque(k);
+        var param_1 = obj.InsertFront(7);
+        var param_2 = obj.DeleteLast();
+        var param_3 = obj.GetFront();
+        var param_4 = obj.InsertLast(5);
+        var param_5 = obj.InsertFront(0);
+        var param_6 = obj.GetFront();
+        var param_7 = obj.GetRear();
+        var param_8 = obj.GetFront();
+        var param_9 = obj.GetFront();
+        var param_10 = obj.GetRear();
+        var param_11 = obj.InsertLast(0);
+
+
     }
 }

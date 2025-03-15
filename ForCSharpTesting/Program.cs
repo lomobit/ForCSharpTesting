3fb1eea4d9d2da7fb89ad4b18e124edcfc1965f7
vghslnk <lomobit@gmail.com>
2025-03-15 13:58:38 +0300

    add commit example with ArrayPool

--- ForCSharpTesting/Program.cs
+++ ForCSharpTesting/Program.cs
@@ -1,4 +1,5 @@
-﻿using ForCSharpTesting.GraviLink;
+﻿using System;
+using System.Buffers;
 using ForCSharpTesting.LeetCodeSolutions.Common;
 
 namespace ForCSharpTesting;
@@ -7,8 +8,51 @@ public class Program
 {
     static void Main(string[] args)
     {
-        //_5_IsFunctionEven.CheckX();
-        _5_IsFunctionEven.RunInternalTests();
-        _5_IsFunctionEven.RunTestsFromFiles();
+        //var newBytes = new byte[100 * 1024 * 1024];
+        //for (int i = 0; i < newBytes.Length; i++)
+        //{
+        //    newBytes[i] = (byte)(i % byte.MaxValue);
+        //}
+        //File.WriteAllBytes(@"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\100MB.dat", newBytes);
+        //return;
+        
+        var smallFilePath = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\bridge3.png";
+        var largeFilePath = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\bridge4.png";
+        var veryLargeFilePath = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\100MB.dat";
+
+
+
+        byte[] bytes = null;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            try
+            {
+                using (var fileStream = new FileStream(veryLargeFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    //bytes = new byte[fileStream.Length];
+                    bytes = ArrayPool<byte>.Shared.Rent((int)fileStream.Length);
+
+                    fileStream.Read(bytes, 0, bytes.Length);
+                }
+            }
+            finally
+            {
+                // Возвращаем массив в пул
+                if (bytes is not null)
+                {
+                    ArrayPool<byte>.Shared.Return(bytes);
+                }
+            }
+        }
+
+        Console.WriteLine($"TotalAllocatedBytes: {GC.GetTotalAllocatedBytes()}");
+        Console.WriteLine($"TotalMemory: {GC.GetTotalMemory(true)}");
+    }
+
+    class A { }
+
+    class B : A
+    {
     }
 }

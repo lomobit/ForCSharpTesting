99b1e6863938aa4eba0227594d4dcbced1cce9d6
vghslnk <lomobit@gmail.com>
2025-03-18 13:59:30 +0300

    commit image generator

--- ForCSharpTesting/Program.cs
+++ ForCSharpTesting/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Buffers;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Pipes;
 using ForCSharpTesting.LeetCodeSolutions.Common;
+using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftAntimalwareEngine;
 
 namespace ForCSharpTesting;
 
@@ -8,52 +13,75 @@ public class Program
 {
     static void Main(string[] args)
     {
-        //var newBytes = new byte[100 * 1024 * 1024];
-        //for (int i = 0; i < newBytes.Length; i++)
-        //{
-        //    newBytes[i] = (byte)(i % byte.MaxValue);
-        //}
-        //File.WriteAllBytes(@"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\100MB.dat", newBytes);
-        //return;
-        
-        var smallFilePath = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\bridge3.png";
+        var path = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\";
+        var smallFilePath = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\bridge1.png";
         var largeFilePath = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\bridge4.png";
-        var veryLargeFilePath = @"D:\Projects\Adve\Code\adve-community-ui\public\images\mocks\location-bridges\100MB.dat";
 
+        int[] mobileSizes = { 320, 375, 425, 480 };
+        int[] tabletSizes = { 640, 768, 960, 1024 };
+        int[] notebookSizes = { 1280, 1366, 1440, 1600, 1920 };
 
+        var sizes = mobileSizes
+            .Concat(tabletSizes)
+            .Concat(notebookSizes)
+            .ToArray();
 
-        byte[] bytes = null;
+        var pool = ArrayPool<Bitmap>.Shared;
 
-        // TODO: попробовать запустить параллельные потоки и посмотреть, как он будет отрабатывать с ними.
-        for (int i = 0; i < 1000; i++)
+        Console.WriteLine($"TotalMemory_before: {GC.GetTotalMemory(true)}");
+        Image image = Image.FromFile(smallFilePath);
+
+        for (int ii = 0; ii < 100; ii++)
         {
-            try
-            {
-                using (var fileStream = new FileStream(veryLargeFilePath, FileMode.Open, FileAccess.Read))
-                {
-                    //bytes = new byte[fileStream.Length];
-                    bytes = ArrayPool<byte>.Shared.Rent((int)fileStream.Length);
+            if (ii % 10 == 0) Console.WriteLine(ii);
+            var filePath = ii % 2 == 0 ? smallFilePath : largeFilePath;
 
-                    fileStream.Read(bytes, 0, bytes.Length);
-                }
-            }
-            finally
+            // Загрузка файла в Stream
+            using (FileStream readerStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
-                // Возвращаем массив в пул
-                if (bytes is not null)
+
+                //Console.WriteLine($"TotalMemory_fileStream: {GC.GetTotalMemory(true)}");
+
+                // Преобразование Stream в Image
+                //using (Image image = Image.FromStream(fileStream))
+                //{
+                //Console.WriteLine($"TotalAllocatedBytes_image: {GC.GetTotalAllocatedBytes()}");
+                //Console.WriteLine($"TotalMemory_image: {GC.GetTotalMemory(true)}");
+                var bitmapArr = pool.Rent(sizes.Length);
+
+                for (int i = 0; i < sizes.Length; i++)
                 {
-                    ArrayPool<byte>.Shared.Return(bytes);
+                    var size = sizes[i];
+                    if (size > image.Width)
+                    {
+                        break;
+                    }
+
+
+                    //var bitmapArr = new Bitmap[1];
+
+                    // Изменение размера изображения
+                    using (bitmapArr[i] = new Bitmap(image, new Size(size, size * image.Height / image.Width)))
+                    {
+
+                        //Console.WriteLine($"\nTotalMemory_newBitmap_{size}: {GC.GetTotalMemory(true)}");
+
+                        // Сохранение в формате JPEG
+                        string outputFilePath = @$"{path}\new\{(ii % 2 == 0 ? "small" : "large")}_image_{size}.jpg";
+                        using (FileStream writerStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+                        {
+                            bitmapArr[i].Save(writerStream, ImageFormat.Jpeg);
+                        }
+
+                    }
                 }
+
+                pool.Return(bitmapArr);
+
+                //}
             }
         }
 
-        Console.WriteLine($"TotalAllocatedBytes: {GC.GetTotalAllocatedBytes()}");
-        Console.WriteLine($"TotalMemory: {GC.GetTotalMemory(true)}");
-    }
-
-    class A { }
-
-    class B : A
-    {
+        Console.WriteLine($"TotalMemory_final: {GC.GetTotalMemory(true)}");
     }
 }

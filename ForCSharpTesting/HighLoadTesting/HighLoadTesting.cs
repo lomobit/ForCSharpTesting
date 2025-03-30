eb0eda68203f04e8bf584eaec32d2f34399fae99
vghslnk <lomobit@gmail.com>
2025-03-30 13:09:16 +0300

    add HighLoadTesting stats

--- ForCSharpTesting/HighLoadTesting/HighLoadTesting.cs
+++ ForCSharpTesting/HighLoadTesting/HighLoadTesting.cs
@@ -1,14 +1,31 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace ForCSharpTesting.HighLoadTesting;
 
 public static class HighLoadTesting
 {
-    private const int Iterations = 100;
-    private const int TasksCount = 10;
+    private const int Iterations = 2; //10;
+    private const int TasksCount = 10; //200;
+
+    const int __30MIN_SECONDS = 30 * 60; // 30 min
+    private static int RPSCount;
+    private static List<int> RPSList;
+    private static List<long> RequestsDurations;
+    private static bool CheckRPS;
 
     public static async Task HighLoadFilesMultiThread()
     {
+        RPSCount = 0;
+        RPSList = new(__30MIN_SECONDS);
+        RequestsDurations = new();
+        CheckRPS = true;
+
+        var RPSThread = new Thread(GetCurrentRPSSnapshot);
+        RPSThread.Start();
+
+
         var tasks = new List<Task>(TasksCount);
         for (int i = 0; i < TasksCount; i++)
         {
@@ -19,6 +36,32 @@ public static class HighLoadTesting
         }
 
         await Task.WhenAll(tasks);
+
+        CheckRPS = false;
+
+        Console.WriteLine(GetHighLoadFilesMultiThreadResult());
+    }
+
+    private static string GetHighLoadFilesMultiThreadResult()
+    {
+        var result = new StringBuilder();
+
+        result.AppendLine();
+        result.AppendLine("====================================================================");
+        result.AppendLine($"~RPS: {RPSList.Average()}");
+        result.AppendLine($"~RequestDuration: {RequestsDurations.Average() / 1000}s");
+
+        return result.ToString();
+    }
+
+    private static void GetCurrentRPSSnapshot()
+    {
+        while (CheckRPS)
+        {
+            RPSList.Add(RPSCount);
+
+            Thread.Sleep(1000);
+        }
     }
 
     public static void HighLoadFiles()
@@ -35,40 +78,45 @@ public static class HighLoadTesting
             "titlePhoto.png",
         ];
 
+        using var client = new HttpClient();
+        var stopwatch = new Stopwatch();
 
         for (int i = 0; i < Iterations; i++)
         {
-            using var client = new HttpClient();
+            RPSCount++;
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: Start request");
+            
 
+            using var form = new MultipartFormDataContent();
+            
             foreach (var fileName in fileNames)
             {
-                using var form = new MultipartFormDataContent();
                 var imagePath = Path.Combine(directoryPath, fileName);
-
                 var fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
                 var fileContent = new StreamContent(fileStream);
                 fileContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
                 form.Add(fileContent, "files", Path.GetFileName(imagePath));
-
-                var request = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Post,
-                    RequestUri = new Uri("http://localhost:4000/adm1h/location/create"),
-                    Content = form,
-                };
-
-                using var response = client.SendAsync(request).ConfigureAwait(false).GetAwaiter().GetResult();
-                response.EnsureSuccessStatusCode();
-                var body = response.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
-
-
-
-                fileContent.Dispose();
-                fileStream.Dispose();
-
-
-                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {body}");
             }
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("http://localhost:4000/adm1h/location/create"),
+                Content = form,
+            };
+
+            stopwatch.Start();
+
+            using var response = client.SendAsync(request).ConfigureAwait(false).GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+            var body = response.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            stopwatch.Stop();
+            RequestsDurations.Add(stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {body}");
+            RPSCount--;
         }
     }
 }
--- ForCSharpTesting/Program.cs
+++ ForCSharpTesting/Program.cs
@@ -6,6 +6,9 @@ public class Program
 {
     static async Task Main(string[] args)
     {
+        //HighLoadTesting.HighLoadTesting.HighLoadFiles();
+        //return;
+
         await HighLoadTesting.HighLoadTesting.HighLoadFilesMultiThread();
     }
 }
